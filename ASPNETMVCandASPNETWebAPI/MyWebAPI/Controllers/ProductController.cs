736f225f8fc347360596a9f58386eeacb5b45c8b
chpadh <chpadh@MININT-LSVN5SD>
2017-02-05 00:29:35 +0530

    ASPNETMVCandASPNETWebAPI

---
+++ ASPNETMVCandASPNETWebAPI/MyWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyWebAPI.Models;
+
+namespace MyWebAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private ProductRepository repo = new ProductRepository();
+
+        public IEnumerable<Product> Get()
+        {
+            return repo.FindAll();
+        }
+
+        public Product Get(string id)
+        {
+            return repo.Find(id);
+        }
+    }
+}
---
+++ ASPNETMVCandASPNETWebAPI/MyWebAPI/Models/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebAPI.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> products = new List<Product>();
+
+        public ProductRepository()
+        {
+            products.Add(new Product { Id = "1", Name = "Product 1", Price = 100 });
+            products.Add(new Product { Id = "2", Name = "Product 2", Price = 200 });
+            products.Add(new Product { Id = "3", Name = "Product 3", Price = 300 });
+            products.Add(new Product { Id = "4", Name = "Product 4", Price = 400 });
+        }
+
+        public IEnumerable<Product> FindAll()
+        {
+            return products;
+        }
+
+        public Product Find(string id)
+        {
+            return products.Single(p => p.Id.Equals(id));
+        }
+    }
+}
